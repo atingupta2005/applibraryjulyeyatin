3b15b4ad4a9c3977dc421edfbff1a9eb0128f611
Atin Gupta <atingupta2005@gmail.com>
2021-06-12 02:36:13 +0000

    Add project files.

---
+++ applibrary/app.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace applibraryjulyeyatin
+{
+    public class app
+    {
+        public void Display()
+        {
+            Console.WriteLine("This is the app class");
+        }
+    }
+}
